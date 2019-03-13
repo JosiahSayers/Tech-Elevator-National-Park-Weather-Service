605d55ca8c9f67a3937a166ea1c9a2dc0058ccb8
Josiah Sayers <josiah.sayers15@gmail.com>
2019-03-13 11:11:34 -0400

    Add System.Data.SqlClient NuGet Dependencies to Tests Project

--- 13-Capstone/CapstoneTests/DAL Tests/Survey_ResultSqlDALTests.cs
+++ 13-Capstone/CapstoneTests/DAL Tests/Survey_ResultSqlDALTests.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
+using System.Transactions;
 
 namespace CapstoneTests.DAL_Tests
 {
+    [TestClass]
     public class Survey_ResultSqlDALTests
     {
+        private TransactionScope tran;
+        private string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=NPGeek;Integrated Security=True";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            tran = new TransactionScope();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            tran.Dispose();
+        }
+
+
     }
 }
